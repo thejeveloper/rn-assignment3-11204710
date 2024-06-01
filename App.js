a1b8bc26ac86048c828aa9f6e42115e6a63a34bd
thejaveloper <tinkorangjames@gmail.com>
2024-06-01 17:03:40 +0000

    App.js component

--- App.js
+++ App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, View, Text } from 'react-native';
+import Header from './components/Header';
+import SearchBar from './components/SearchBar';
+import Categories from './components/Categories';
+import OngoingTasks from './components/OngoingTasks';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>My name is <Text style={styles.bold}>Tinkorang James</Text></Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <Header />
+      <SearchBar />
+      <Categories />
+      <OngoingTasks />
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'brown', 
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 24, 
-  },
-  bold: {
-    fontWeight: 'bold', 
+    padding: 20,
+    backgroundColor: '#f5f5f5',
   },
 });
+
